06f388abc74489cf95e13b334311cf440b540125
Giacinto <g.tucciarone@solvingteam.it>
2020-07-31 10:54:00 +0200

    ENG-761: Field message deleted in the CSRFProtectionException class

--- src/main/java/org/entando/entando/aps/system/exception/CSRFProtectionException.java
+++ src/main/java/org/entando/entando/aps/system/exception/CSRFProtectionException.java
@@ -2,18 +2,10 @@ package org.entando.entando.aps.system.exception;
 
 public class CSRFProtectionException extends RuntimeException {
 
-    private String message;
-
     public CSRFProtectionException(String message) {
         super(message);
     }
-
-    @Override
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
+    public CSRFProtectionException(String message,Throwable e) {
+        super(message,e);
     }
 }
