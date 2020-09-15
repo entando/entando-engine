d3785013efcf7cc3b30e2067ea85728a605ee0d2
lucorset@gmail.com <burnl0nd0nburn!>
2020-09-15 10:15:25 +0200

    ENG-703 swagger ui enabled by default with clientid swagger_ui

--- src/main/java/org/entando/entando/web/swagger/SwaggerConfig.java
+++ src/main/java/org/entando/entando/web/swagger/SwaggerConfig.java
@@ -1,104 +1,108 @@
 package org.entando.entando.web.swagger;
 
-import com.agiletec.aps.system.SystemConstants;
-import com.agiletec.aps.system.services.baseconfig.BaseConfigManager;
-import com.fasterxml.classmate.TypeResolver;
-import java.lang.reflect.WildcardType;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.Ordered;
-import springfox.documentation.schema.AlternateTypeRules;
-import org.entando.entando.web.user.model.UserAuthoritiesRequest;
-import org.springframework.context.annotation.*;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.builders.*;
-import springfox.documentation.service.*;
+import java.util.Collections;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.AuthorizationCodeGrantBuilder;
+import springfox.documentation.builders.OAuthBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.GrantType;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
+import springfox.documentation.service.TokenEndpoint;
+import springfox.documentation.service.TokenRequestEndpoint;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Map;
-import java.util.List;
-
-import static java.util.Collections.singletonList;
-
+@Configuration
 @ComponentScan
 @EnableSwagger2
-@EnableWebMvc
 public class SwaggerConfig {
 
-    private static final String ENTANDO_AUTH = "entando_auth";
+    private static final String REFERENCE_NAME = "entando";
+    private static final String swaggerUi = "swagger_ui";
 
-    @Autowired
-    private TypeResolver typeResolver;
+    private final String clientId;
+    private final String clientSecret;
+    private final String authServer;
 
-    private BaseConfigManager baseConfigManager;
+    public SwaggerConfig() {
 
-    public SwaggerConfig(BaseConfigManager baseConfigManager) {
-        this.baseConfigManager = baseConfigManager;
+        this.authServer = "http://keb-kc-keb.apps.rd.entando.org/auth/realms/entando/protocol/openid-connect";
+        this.clientId = swaggerUi;
+        this.clientSecret = swaggerUi;
     }
 
     @Bean
     public Docket api() {
+
         return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("entando")
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo())
-                .securitySchemes(singletonList(oAuth()))
-                .securityContexts(singletonList(securityContext()))
-                .directModelSubstitute(UserAuthoritiesRequest.class, String.class)
-                .alternateTypeRules(
-                        // This rule is necessary to allow Swagger resolving Map<String, List<String>> types
-                        AlternateTypeRules.newRule(
-                                typeResolver.resolve(Map.class, String.class, typeResolver.resolve(List.class, String.class)),
-                                typeResolver.resolve(Map.class, String.class, WildcardType.class), Ordered.HIGHEST_PRECEDENCE)
-                        );
+                .securitySchemes((getSecuritySchemes()))
+                .securityContexts(securityContext());
     }
 
-    private OAuth oAuth() {
-        return new OAuthBuilder()
-                .name(ENTANDO_AUTH)
-                .grantTypes(grantTypes())
-                .scopes(singletonList(globalScope()))
+    private List<SecurityScheme> getSecuritySchemes() {
+
+        SecurityScheme oAuth = new OAuthBuilder()
+                .name(REFERENCE_NAME)
+                .grantTypes(getGrantTypes())
                 .build();
+
+        return Collections.singletonList(oAuth);
     }
 
-    private List<GrantType> grantTypes() {
-        String authUrl = baseConfigManager.getParam(SystemConstants.PAR_APPL_BASE_URL);
+    private List<GrantType> getGrantTypes() {
 
-        return singletonList(new ResourceOwnerPasswordCredentialsGrant(authUrl + "api/oauth/token"));
-    }
-
-    private AuthorizationScope globalScope() {
-        return new AuthorizationScope("global", "accessEverything");
-    }
-
-    private SecurityContext securityContext() {
-        return SecurityContext.builder()
-                .securityReferences(defaultAuth())
-                .forPaths(PathSelectors.regex("/.*"))//<17>
+        GrantType grantType = new AuthorizationCodeGrantBuilder()
+                .tokenEndpoint(new TokenEndpoint(this.authServer + "/token", "oauthtoken"))
+                .tokenRequestEndpoint(
+                        new TokenRequestEndpoint(this.authServer + "/auth", this.clientId, this.clientSecret))
                 .build();
+
+        return Collections.singletonList(grantType);
     }
 
-    private List<SecurityReference> defaultAuth() {
-        AuthorizationScope globalScope
-                = new AuthorizationScope("global", "Access everything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = globalScope;
+    private List<SecurityContext> securityContext() {
 
-        return singletonList(SecurityReference.builder()
-                .reference(ENTANDO_AUTH)
-                .scopes(authorizationScopes)
-                .build());
+        SecurityContext securityContext = SecurityContext.builder()
+                .securityReferences(getDefaultAuths())
+                .forPaths(PathSelectors.regex("/.*"))
+                .build();
+
+        return Collections.singletonList(securityContext);
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Entando Rest API")
-                .description("Entando AppBuilder Rest APIs")
-                .contact(new Contact("Entando", "http://www.entando.com", null))
+    private List<SecurityReference> getDefaultAuths() {
+
+        SecurityReference securityReference = SecurityReference.builder()
+                .reference(REFERENCE_NAME)
+                .scopes(new AuthorizationScope[0])
+                .build();
+
+        return Collections.singletonList(securityReference);
+    }
+
+
+    @Bean
+    public SecurityConfiguration security() {
+
+        return SecurityConfigurationBuilder.builder()
+                .clientId(this.clientId)
+                .clientSecret(this.clientSecret)
+                .useBasicAuthenticationWithAccessCodeGrant(true)
                 .build();
     }
 }
