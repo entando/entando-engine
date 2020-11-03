63706d6d89c0ec70e6ac5d6216461a9c4da7431f
Alessio Pintus <alessio.pintus@gmail.com>
2020-11-03 15:11:16 +0100

    ENG-1276 tests improvements

--- src/test/java/com/agiletec/aps/system/services/widgettype/TestWidgetType.java
+++ src/test/java/com/agiletec/aps/system/services/widgettype/TestWidgetType.java
@@ -6,6 +6,10 @@ import org.entando.entando.aps.system.services.widgettype.WidgetType;
 import org.entando.entando.ent.exception.EntException;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
 public class TestWidgetType extends BaseTestCase {
 
     @Override
@@ -15,24 +19,32 @@ public class TestWidgetType extends BaseTestCase {
 
     @Test
     public void testEqualsAndHashcode() throws EntException {
-        String widgetTypeCode = "test_showletType";
-        WidgetType widgetType1 = createWidgetType(widgetTypeCode);
-        WidgetType widgetType2 = createWidgetType(widgetTypeCode);
-        assertEquals(widgetType1, widgetType2);
-        assertEquals(widgetType1.hashCode(),widgetType2.hashCode());
-    }
-
-    private WidgetType createWidgetType(String code) {
-        WidgetType type = new WidgetType();
-        type.setCode(code);
+        String widgetTypeCode1 = "testWidgetType1";
+        String widgetTypeCode2 = "testWidgetType2";
+        String widgetCategory1 = "test";
+        String widgetCategory2 = "category";
         ApsProperties titles = new ApsProperties();
         titles.put("it", "Titolo in ITA");
         titles.put("en", "Title in ENG");
-        type.setTitles(titles);
         ApsProperties config = new ApsProperties();
+        WidgetType widgetType1 = createWidgetType(widgetTypeCode1, titles, config, true, widgetCategory1);
+        WidgetType widgetType2 = createWidgetType(widgetTypeCode1, titles, config, true, widgetCategory1);
+        WidgetType widgetType3 = createWidgetType(widgetTypeCode2, titles, config, false, widgetCategory2);
+        assertThat(widgetType1.equals(widgetType2), is(true));
+        assertThat(widgetType2.equals(widgetType1), is(true));
+        assertThat(widgetType1.equals(widgetType3), is(false));
+        assertThat(widgetType3.equals(widgetType1), is(false));
+        assertThat(widgetType1.hashCode(), is(not(widgetType3.hashCode())));
+        assertThat(widgetType1.hashCode(), is(widgetType2.hashCode()));
+    }
+
+    private WidgetType createWidgetType(String code, ApsProperties titles, ApsProperties config, Boolean readonlyPageWidgetConfig, String widgetCategory) {
+        WidgetType type = new WidgetType();
+        type.setCode(code);
+        type.setTitles(titles);
         type.setConfig(config);
-        type.setWidgetCategory("test");
-        type.setReadonlyPageWidgetConfig(false);
+        type.setWidgetCategory(widgetCategory);
+        type.setReadonlyPageWidgetConfig(readonlyPageWidgetConfig);
         return type;
     }
 }
