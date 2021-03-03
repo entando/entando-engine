03fa4ef4504f5d454e4b742708167c928a61c0f1
Alessio Pintus <alessio.pintus@gmail.com>
2021-03-03 17:41:44 +0100

    ENG-1918 add GET myProfile API

--- src/main/java/org/entando/entando/web/userprofile/ProfileController.java
+++ src/main/java/org/entando/entando/web/userprofile/ProfileController.java
@@ -81,6 +81,19 @@ public class ProfileController {
     @RequestMapping(value = "/userProfiles/{username}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<SimpleRestResponse<EntityDto>> getUserProfile(@PathVariable String username) throws JsonProcessingException {
         logger.debug("Requested profile -> {}", username);
+        final EntityDto dto = getUserProfileEntityDto(username);
+        logger.debug("Main Response -> {}", dto);
+        return new ResponseEntity<>(new SimpleRestResponse<>(dto), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/userProfiles/myProfile", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<SimpleRestResponse<EntityDto>> getMyProfile(@ModelAttribute("user") UserDetails user) {
+        final EntityDto userProfileEntityDto = getUserProfileEntityDto(user.getUsername());
+        logger.debug("Main Response -> {}", userProfileEntityDto);
+        return new ResponseEntity<>(new SimpleRestResponse<>(userProfileEntityDto), HttpStatus.OK);
+    }
+
+    private EntityDto getUserProfileEntityDto(final String username) {
         EntityDto dto;
         if (!this.getProfileValidator().existProfile(username)) {
             if (userExists(username)) {
@@ -93,8 +106,7 @@ public class ProfileController {
         } else {
             dto = this.getUserProfileService().getUserProfile(username);
         }
-        logger.debug("Main Response -> {}", dto);
-        return new ResponseEntity<>(new SimpleRestResponse<>(dto), HttpStatus.OK);
+        return dto;
     }
 
     private boolean userExists(String username) {
