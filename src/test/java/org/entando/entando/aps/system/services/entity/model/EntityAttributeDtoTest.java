eb08c8459bcdee5e42c61c1046d1dbea494d6792
Alessio Pintus <alessio.pintus@gmail.com>
2021-04-27 17:13:19 +0200

    ENG-2251 test code improvements

--- src/test/java/org/entando/entando/aps/system/services/entity/model/EntityAttributeDtoTest.java
+++ src/test/java/org/entando/entando/aps/system/services/entity/model/EntityAttributeDtoTest.java
@@ -1,35 +1,32 @@
 package org.entando.entando.aps.system.services.entity.model;
 
-import com.agiletec.aps.system.common.entity.model.attribute.AttributeInterface;
 import com.agiletec.aps.system.common.entity.model.attribute.BooleanAttribute;
-import org.entando.entando.ent.util.EntLogging;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class EntityAttributeDtoTest {
-    private final EntLogging.EntLogger logger = EntLogging.EntLogFactory.getSanitizedLogger(getClass());
-
-    private EntityAttributeDto entityAttributeDto;
+class EntityAttributeDtoTest {
 
     @Test
     void testFillEntityAttributeBoolean() {
-        AttributeInterface attribute = new BooleanAttribute();
+        EntityAttributeDto entityAttributeDto;
+
+        BooleanAttribute attribute = new BooleanAttribute();
         entityAttributeDto = new EntityAttributeDto(attribute);
 
         entityAttributeDto.setValue("true");
         entityAttributeDto.fillEntityAttribute(attribute, null);
-        Assertions.assertTrue(((BooleanAttribute) attribute).getBooleanValue());
+        Assertions.assertTrue(attribute.getBooleanValue());
 
         entityAttributeDto.setValue("false");
         entityAttributeDto.fillEntityAttribute(attribute, null);
-        Assertions.assertFalse(((BooleanAttribute) attribute).getBooleanValue());
+        Assertions.assertFalse(attribute.getBooleanValue());
 
         entityAttributeDto.setValue(true);
         entityAttributeDto.fillEntityAttribute(attribute, null);
-        Assertions.assertTrue(((BooleanAttribute) attribute).getBooleanValue());
+        Assertions.assertTrue(attribute.getBooleanValue());
 
         entityAttributeDto.setValue(false);
         entityAttributeDto.fillEntityAttribute(attribute, null);
-        Assertions.assertFalse(((BooleanAttribute) attribute).getBooleanValue());
+        Assertions.assertFalse(attribute.getBooleanValue());
     }
 }
