2b9340b6980ee3f18dd11015a31629c0dd126a29
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2021-07-01 17:07:25 +0200

    ENG-2312: Removed unused import

--- src/main/java/com/agiletec/aps/tags/URLTag.java
+++ src/main/java/com/agiletec/aps/tags/URLTag.java
@@ -21,7 +21,6 @@ import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.TagSupport;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.entando.entando.ent.util.EntLogging.EntLogger;
 import org.entando.entando.ent.util.EntLogging.EntLogFactory;
 
