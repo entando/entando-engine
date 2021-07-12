8eaf5ae60a219efb4acc3f83bde42d53ffa6a419
Breno Queiroz <BrenoQVDS>
2021-07-12 18:23:18 -0300

    ENG-2178 Fixing swagger URL, hidding unwanted fields and suppressing snyk vulnerability

--- src/main/java/org/entando/entando/web/swagger/SwaggerConfig.java
+++ src/main/java/org/entando/entando/web/swagger/SwaggerConfig.java
@@ -1,16 +1,21 @@
 package org.entando.entando.web.swagger;
 
 import com.agiletec.aps.system.SystemConstants;
+import com.agiletec.aps.system.common.FieldSearchFilter;
+import com.agiletec.aps.system.common.entity.model.EntitySearchFilter;
 import com.fasterxml.classmate.TypeResolver;
 import java.lang.reflect.WildcardType;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import javax.servlet.ServletContext;
 import org.entando.entando.web.user.model.UserAuthoritiesRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
+import org.springframework.web.util.UriComponentsBuilder;
 import springfox.documentation.builders.AuthorizationCodeGrantBuilder;
 import springfox.documentation.builders.OAuthBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -24,13 +29,15 @@ import springfox.documentation.service.TokenEndpoint;
 import springfox.documentation.service.TokenRequestEndpoint;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.paths.DefaultPathProvider;
+import springfox.documentation.spring.web.paths.Paths;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @ComponentScan
-@EnableSwagger2
+@EnableSwagger2WebMvc
 public class SwaggerConfig {
 
     private static final String REFERENCE_NAME = "entando";
@@ -41,6 +48,9 @@ public class SwaggerConfig {
     private String kcClientId;
     private String kcClientSecret;
     private String authServer;
+
+    @Autowired
+    private ServletContext servletContext;
 
     public SwaggerConfig(Environment environment, TypeResolver typeResolver) {
 
@@ -56,7 +66,6 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-
         return new Docket(DocumentationType.SWAGGER_2)
                 .groupName(GROUP_NAME)
                 .select()
@@ -65,6 +74,7 @@ public class SwaggerConfig {
                 .build()
                 .securitySchemes((getSecuritySchemes()))
                 .securityContexts(securityContext())
+                .pathProvider(new EntandoPathProvider())
                 .directModelSubstitute(UserAuthoritiesRequest.class, String.class)
                 .alternateTypeRules(
                         // This rule is necessary to allow Swagger resolving Map<String, List<String>> types
@@ -123,4 +133,17 @@ public class SwaggerConfig {
                 .useBasicAuthenticationWithAccessCodeGrant(true)
                 .build();
     }
+
+    private class EntandoPathProvider extends DefaultPathProvider {
+        @Override
+        public String getOperationPath(String operationPath) {
+            String contextPath = servletContext.getContextPath();
+            if (operationPath.startsWith(contextPath)) {
+                operationPath = operationPath.substring(contextPath.length());
+            }
+            return Paths.removeAdjacentForwardSlashes(UriComponentsBuilder.newInstance().replacePath(operationPath)
+                    .build().toString());
+        }
+    }
+
 }
