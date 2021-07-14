176bd4ee1d9596869c2bdb5e724727b7ce95974e
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2021-07-14 13:12:31 +0200

    ENG-2552: Fix code smell

--- src/main/java/org/entando/entando/aps/util/crypto/CryptoBeansConfig.java
+++ src/main/java/org/entando/entando/aps/util/crypto/CryptoBeansConfig.java
@@ -15,7 +15,6 @@ package org.entando.entando.aps.util.crypto;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
