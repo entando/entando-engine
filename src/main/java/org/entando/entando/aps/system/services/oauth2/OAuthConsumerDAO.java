5d8922296af0efc0b37ee9270a4db3dcce25f817
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2021-10-21 09:48:06 +0200

    ENG-2188: Upgrade datetime fields and "scope" column

--- src/main/java/org/entando/entando/aps/system/services/oauth2/OAuthConsumerDAO.java
+++ src/main/java/org/entando/entando/aps/system/services/oauth2/OAuthConsumerDAO.java
@@ -41,17 +41,17 @@ public class OAuthConsumerDAO extends AbstractSearcherDAO implements IOAuthConsu
     private static final EntLogger logger = EntLogFactory.getSanitizedLogger(OAuthConsumerDAO.class);
 
     private static final String SELECT_CONSUMER
-            = "SELECT consumerkey, consumersecret, name, description, callbackurl, scopes, authorizedgranttypes, expirationdate, issueddate "
+            = "SELECT consumerkey, consumersecret, name, description, callbackurl, scope, authorizedgranttypes, expirationdate, issueddate "
             + "FROM api_oauth_consumers WHERE consumerkey = ? ";
 
     private static final String ADD_CONSUMER
-            = "INSERT INTO api_oauth_consumers (consumerkey, consumersecret, name, description, callbackurl, scopes, authorizedgranttypes, expirationdate, issueddate) VALUES (?,?,?,?,?,?,?,?,?) ";
+            = "INSERT INTO api_oauth_consumers (consumerkey, consumersecret, name, description, callbackurl, scope, authorizedgranttypes, expirationdate, issueddate) VALUES (?,?,?,?,?,?,?,?,?) ";
 
     private static final String UPDATE_CONSUMER_PREFIX = "UPDATE api_oauth_consumers SET ";
 
     private static final String UPDATE_CONSUMER_SECRET = "consumersecret = ? , ";
 
-    private static final String UPDATE_CONSUMER_SUFFIX = "name = ? , description = ? , callbackurl = ?, scopes = ?, authorizedgranttypes = ?, expirationdate = ? WHERE consumerkey = ? ";
+    private static final String UPDATE_CONSUMER_SUFFIX = "name = ? , description = ? , callbackurl = ?, scope = ?, authorizedgranttypes = ?, expirationdate = ? WHERE consumerkey = ? ";
 
     private static final String UPDATE_CONSUMER = UPDATE_CONSUMER_PREFIX + UPDATE_CONSUMER_SUFFIX;
 
@@ -84,6 +84,7 @@ public class OAuthConsumerDAO extends AbstractSearcherDAO implements IOAuthConsu
                 consumer = consumerFromResultSet(res);
             }
         } catch (SQLException | EntException t) {
+            t.printStackTrace();
             logger.error("Error while loading consumer by clientid {}", clientId, t);
             throw new RuntimeException("Error while loading consumer by key " + clientId, t);
         } finally {
@@ -140,7 +141,7 @@ public class OAuthConsumerDAO extends AbstractSearcherDAO implements IOAuthConsu
         consumer.setName(res.getString("name"));
         consumer.setDescription(res.getString("description"));
         consumer.setAuthorizedGrantTypes(res.getString("authorizedgranttypes"));
-        consumer.setScope(res.getString("scopes"));
+        consumer.setScope(res.getString("scope"));
         consumer.setExpirationDate(res.getTimestamp("expirationdate"));
         consumer.setIssuedDate(res.getTimestamp("issueddate"));
         return consumer;
