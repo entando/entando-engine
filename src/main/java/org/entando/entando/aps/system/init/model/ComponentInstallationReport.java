17095b8b2aceffeac948b541542c4e1a120aa7f4
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2021-11-23 11:08:48 +0100

    ENG-2633: reformat code

--- src/main/java/org/entando/entando/aps/system/init/model/ComponentInstallationReport.java
+++ src/main/java/org/entando/entando/aps/system/init/model/ComponentInstallationReport.java
@@ -24,138 +24,146 @@ import org.jdom.Element;
  * @author E.Santoboni
  */
 public class ComponentInstallationReport implements Serializable {
-	
-	private ComponentInstallationReport() {}
-	
-	protected ComponentInstallationReport(Element element) {
-		String componentCode = element.getAttributeValue(SystemInstallationReport.CODE_ATTRIBUTE);
-		this.setComponentCode(componentCode);
-		String dateString = element.getAttributeValue(SystemInstallationReport.DATE_ATTRIBUTE);
-		Date date = DateConverter.parseDate(dateString, SystemInstallationReport.DATE_FORMAT);
-		this.setDate(date);
-		Element dataElement = element.getChild(SystemInstallationReport.DATA_ELEMENT);
-		if (null != dataElement) {
-			this.setDataReport(new DataInstallationReport(dataElement));
-		} else {
+
+    private ComponentInstallationReport() {
+    }
+
+    protected ComponentInstallationReport(Element element) {
+        String componentCode = element.getAttributeValue(SystemInstallationReport.CODE_ATTRIBUTE);
+        this.setComponentCode(componentCode);
+        String dateString = element.getAttributeValue(SystemInstallationReport.DATE_ATTRIBUTE);
+        Date date = DateConverter.parseDate(dateString, SystemInstallationReport.DATE_FORMAT);
+        this.setDate(date);
+        Element dataElement = element.getChild(SystemInstallationReport.DATA_ELEMENT);
+        if (null != dataElement) {
+            this.setDataReport(new DataInstallationReport(dataElement));
+        } else {
             this.setDataReport(new DataInstallationReport());
         }
-		Element liquibaseElement = element.getChild(SystemInstallationReport.LIQUIBASE_ELEMENT);
-		if (null != liquibaseElement) {
-			this.setLiquibaseReport(new LiquibaseInstallationReport(liquibaseElement));
-		} else {
+        Element liquibaseElement = element.getChild(SystemInstallationReport.LIQUIBASE_ELEMENT);
+        if (null != liquibaseElement) {
+            this.setLiquibaseReport(new LiquibaseInstallationReport(liquibaseElement));
+        } else {
             this.setLiquibaseReport(new LiquibaseInstallationReport());
         }
-		Element postProcessElement = element.getChild(SystemInstallationReport.COMPONENT_POST_PROCESS_ELEMENT);
-		if (null != postProcessElement) {
-			String postProcessStatusString = postProcessElement.getAttributeValue(SystemInstallationReport.STATUS_ATTRIBUTE);
-			if (null != postProcessStatusString) {
-				SystemInstallationReport.Status postProcessStatus =
-						Enum.valueOf(SystemInstallationReport.Status.class, postProcessStatusString.toUpperCase());
-				this.setPostProcessStatus(postProcessStatus);
-			}
-		}
-	}
+        Element postProcessElement = element.getChild(SystemInstallationReport.COMPONENT_POST_PROCESS_ELEMENT);
+        if (null != postProcessElement) {
+            String postProcessStatusString = postProcessElement
+                    .getAttributeValue(SystemInstallationReport.STATUS_ATTRIBUTE);
+            if (null != postProcessStatusString) {
+                SystemInstallationReport.Status postProcessStatus =
+                        Enum.valueOf(SystemInstallationReport.Status.class, postProcessStatusString.toUpperCase());
+                this.setPostProcessStatus(postProcessStatus);
+            }
+        }
+    }
 
-	public static ComponentInstallationReport getInstance(String componentCode) {
-		ComponentInstallationReport report = new ComponentInstallationReport();
-		report.setDate(new Date());
-		report.setComponentCode(componentCode);
-		report.setDataReport(new DataInstallationReport());
-		report.setLiquibaseReport(new LiquibaseInstallationReport());
-		return report;
-	}
+    public static ComponentInstallationReport getInstance(String componentCode) {
+        ComponentInstallationReport report = new ComponentInstallationReport();
+        report.setDate(new Date());
+        report.setComponentCode(componentCode);
+        report.setDataReport(new DataInstallationReport());
+        report.setLiquibaseReport(new LiquibaseInstallationReport());
+        return report;
+    }
 
-	protected Element toJdomElement() {
-		Element element = new Element(SystemInstallationReport.COMPONENT_ELEMENT);
-		element.setAttribute(SystemInstallationReport.CODE_ATTRIBUTE, this.getComponentCode());
-		String dateString = DateConverter.getFormattedDate(this.getDate(), SystemInstallationReport.DATE_FORMAT);
-		element.setAttribute(SystemInstallationReport.DATE_ATTRIBUTE, dateString);
-		if (null != this.getStatus()) {
-			element.setAttribute(SystemInstallationReport.STATUS_ATTRIBUTE, this.getStatus().toString());
-		}
-		Element dataElement = this.getDataReport().toJdomElement();
-		element.addContent(dataElement);
+    protected Element toJdomElement() {
+        Element element = new Element(SystemInstallationReport.COMPONENT_ELEMENT);
+        element.setAttribute(SystemInstallationReport.CODE_ATTRIBUTE, this.getComponentCode());
+        String dateString = DateConverter.getFormattedDate(this.getDate(), SystemInstallationReport.DATE_FORMAT);
+        element.setAttribute(SystemInstallationReport.DATE_ATTRIBUTE, dateString);
+        if (null != this.getStatus()) {
+            element.setAttribute(SystemInstallationReport.STATUS_ATTRIBUTE, this.getStatus().toString());
+        }
+        Element dataElement = this.getDataReport().toJdomElement();
+        element.addContent(dataElement);
         if (null != this.getLiquibaseReport()) {
             Element liquibaseElement = this.getLiquibaseReport().toJdomElement();
             element.addContent(liquibaseElement);
         }
-		if (null != this.getPostProcessStatus()) {
-			Element postProcessElement = new Element(SystemInstallationReport.COMPONENT_POST_PROCESS_ELEMENT);
-			postProcessElement.setAttribute(SystemInstallationReport.STATUS_ATTRIBUTE, this.getPostProcessStatus().toString());
-			element.addContent(postProcessElement);
-		}
-		return element;
-	}
+        if (null != this.getPostProcessStatus()) {
+            Element postProcessElement = new Element(SystemInstallationReport.COMPONENT_POST_PROCESS_ELEMENT);
+            postProcessElement
+                    .setAttribute(SystemInstallationReport.STATUS_ATTRIBUTE, this.getPostProcessStatus().toString());
+            element.addContent(postProcessElement);
+        }
+        return element;
+    }
 
-	public SystemInstallationReport.Status getStatus() {
-		SystemInstallationReport.Status dataStatus = this.getDataReport().getStatus();
-		boolean isDataStatusSafe = SystemInstallationReport.isSafeStatus(dataStatus);
-		SystemInstallationReport.Status postProcessStatus = this.getPostProcessStatus();
-		boolean isPostProcessStatusSafe = SystemInstallationReport.isSafeStatus(postProcessStatus);
-		if (!isDataStatusSafe ||
-				(!isPostProcessStatusSafe && !postProcessStatus.equals(SystemInstallationReport.Status.INIT))) {
-			return SystemInstallationReport.Status.INCOMPLETE;
-		} else if (isPostProcessStatusSafe) {
-			if (null != dataStatus && SystemInstallationReport.Status.UNINSTALLED.equals(dataStatus)) {
-				return SystemInstallationReport.Status.UNINSTALLED;
-			} else {
-				return SystemInstallationReport.Status.OK;
-			}
-		} else {
-			return SystemInstallationReport.Status.INIT;
-		}
-	}
+    public SystemInstallationReport.Status getStatus() {
+        SystemInstallationReport.Status dataStatus = this.getDataReport().getStatus();
+        boolean isDataStatusSafe = SystemInstallationReport.isSafeStatus(dataStatus);
+        SystemInstallationReport.Status postProcessStatus = this.getPostProcessStatus();
+        boolean isPostProcessStatusSafe = SystemInstallationReport.isSafeStatus(postProcessStatus);
+        if (!isDataStatusSafe ||
+                (!isPostProcessStatusSafe && !postProcessStatus.equals(SystemInstallationReport.Status.INIT))) {
+            return SystemInstallationReport.Status.INCOMPLETE;
+        } else if (isPostProcessStatusSafe) {
+            if (null != dataStatus && SystemInstallationReport.Status.UNINSTALLED.equals(dataStatus)) {
+                return SystemInstallationReport.Status.UNINSTALLED;
+            } else {
+                return SystemInstallationReport.Status.OK;
+            }
+        } else {
+            return SystemInstallationReport.Status.INIT;
+        }
+    }
 
-	public boolean isPostProcessExecutionRequired() {
-		SystemInstallationReport.Status dataStatus = this.getDataReport().getStatus();
-		SystemInstallationReport.Status ok = SystemInstallationReport.Status.OK;
-		return (dataStatus.equals(ok) && !this.getDataReport().isDataAlreadyPresent());
-	}
+    public boolean isPostProcessExecutionRequired() {
+        SystemInstallationReport.Status dataStatus = this.getDataReport().getStatus();
+        SystemInstallationReport.Status ok = SystemInstallationReport.Status.OK;
+        return (dataStatus.equals(ok) && !this.getDataReport().isDataAlreadyPresent());
+    }
 
-	public boolean isUninstalled() {
-		return this.getStatus().equals(Status.UNINSTALLED);
-	}
+    public boolean isUninstalled() {
+        return this.getStatus().equals(Status.UNINSTALLED);
+    }
 
-	public String getComponentCode() {
-		return _componentCode;
-	}
-	public void setComponentCode(String componentCode) {
-		this._componentCode = componentCode;
-	}
+    public String getComponentCode() {
+        return _componentCode;
+    }
 
-	public Date getDate() {
-		return _date;
-	}
-	protected void setDate(Date date) {
-		this._date = date;
-	}
+    public void setComponentCode(String componentCode) {
+        this._componentCode = componentCode;
+    }
 
-	public Status getPostProcessStatus() {
-		return _postProcessStatus;
-	}
-	public void setPostProcessStatus(Status postProcessStatus) {
-		this._postProcessStatus = postProcessStatus;
-	}
+    public Date getDate() {
+        return _date;
+    }
 
-	public DataInstallationReport getDataReport() {
-		return _dataReport;
-	}
-	private void setDataReport(DataInstallationReport dataReport) {
-		this._dataReport = dataReport;
-	}
+    protected void setDate(Date date) {
+        this._date = date;
+    }
 
-	public LiquibaseInstallationReport getLiquibaseReport() {
-		return liquibaseReport;
-	}
-	private void setLiquibaseReport(LiquibaseInstallationReport liquibaseReport) {
-		this.liquibaseReport = liquibaseReport;
-	}
+    public Status getPostProcessStatus() {
+        return _postProcessStatus;
+    }
 
-	private String _componentCode;
-	private Date _date;
-	private SystemInstallationReport.Status _postProcessStatus = SystemInstallationReport.Status.INIT;
-	
-	private DataInstallationReport _dataReport;
-	private LiquibaseInstallationReport liquibaseReport;
-	
+    public void setPostProcessStatus(Status postProcessStatus) {
+        this._postProcessStatus = postProcessStatus;
+    }
+
+    public DataInstallationReport getDataReport() {
+        return _dataReport;
+    }
+
+    private void setDataReport(DataInstallationReport dataReport) {
+        this._dataReport = dataReport;
+    }
+
+    public LiquibaseInstallationReport getLiquibaseReport() {
+        return liquibaseReport;
+    }
+
+    private void setLiquibaseReport(LiquibaseInstallationReport liquibaseReport) {
+        this.liquibaseReport = liquibaseReport;
+    }
+
+    private String _componentCode;
+    private Date _date;
+    private SystemInstallationReport.Status _postProcessStatus = SystemInstallationReport.Status.INIT;
+
+    private DataInstallationReport _dataReport;
+    private LiquibaseInstallationReport liquibaseReport;
+
 }
